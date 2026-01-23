7fee59209a427b4b4f4b16409f0980b20fe984dd
Aadhar Singh Bhadauria <aadharsinghbhadauria@Aadhars-MacBook-Air.local>
2026-01-23 19:21:33 +0530

    feat: updating docs CI workflow

--- EfficientAI-Docs/src/pages/index.js
+++ EfficientAI-Docs/src/pages/index.js
@@ -1,30 +1,6 @@
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <main style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '80vh',
-        textAlign: 'center'
-      }}>
-        <h1>EfficientAI Documentation</h1>
-        <p>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Go to Documentation
-          </Link>
-        </p>
-      </main>
-    </Layout>
-  );
+  return <Redirect to="/docs/intro" />;
 }
